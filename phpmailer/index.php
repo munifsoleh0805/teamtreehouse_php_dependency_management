69a2bbc4c36ce5e0ea4e4875d1e0da8d904046f6
munifsoleh0805 <70565611+munifsoleh0805@users.noreply.github.com>
2020-10-20 10:35:30 +0700

    update index.php

--- phpmailer/index.php
+++ phpmailer/index.php
@@ -95,38 +95,3 @@ if (!$mail->send()) {
 }
 
 ?>
-<!-- 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alert Kembali</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="col-5">
-                <?php if(!$mail->send()) : ?>
-                        <div class="alert alert-danger" role="alert">
-                            <?php echo "Error Form Data wajib Diisi !!!" ?>
-                        </div>
-
-                    <?php else  :?>
-                        <div class="alert alert-success" role="alert">
-                            <?php echo "Selesai Data Terkirim " ?>
-                        </div>
-                <?php endif ?>
-                </div>
-                <button class="btn btn-outline-primary"><a href="send.php">Kirim Lagi</a></button>
-            </div>
-        </div>
-    </div>
-
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-</body>
-</html> -->
